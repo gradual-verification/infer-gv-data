83e917a6a5e3fc4278893fa6fb05a7ad857d7973
Sam Estep <sam@samestep.com>
2019-08-15 11:47:55 -0400

    Add some mysteriously missing explain.txt files

--- explain.sh
+++ explain.sh
@@ -5,7 +5,12 @@ python3 explain.py caffeine > caffeine/explain.txt
 python3 explain.py ColdSnap > ColdSnap/explain.txt
 python3 explain.py filesystem-generator > filesystem-generator/explain.txt
 python3 explain.py FloatingActionButtonSpeedDial > FloatingActionButtonSpeedDial/explain.txt
+python3 explain.py jib > jib/explain.txt
 python3 explain.py keyvaluestore > keyvaluestore/explain.txt
+python3 explain.py meal-planner > meal-planner/explain.txt
 python3 explain.py OANDAFX > OANDAFX/explain.txt
+python3 explain.py okbuck > okbuck/explain.txt
+python3 explain.py picasso > picasso/explain.txt
+python3 explain.py QRContact > QRContact/explain.txt
 python3 explain.py skaffold-tools-for-java > skaffold-tools-for-java/explain.txt
 python3 explain.py uLeak > uLeak/explain.txt
