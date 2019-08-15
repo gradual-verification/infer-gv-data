77d5935637ce5c433485edb2de7c47b4afcea3bc
Sam Estep <sam@samestep.com>
2019-08-15 10:33:03 -0400

    Get the line numbers not shared across checkers

---
+++ explain.py
@@ -0,0 +1,36 @@
+import sys
+
+from common import *
+
+def others(checker):
+    cloned_checkers = list(checkers)
+    cloned_checkers.remove(checker)
+    return cloned_checkers
+
+def explain(repo):
+    sets = {}
+    for name, issuetype in checkers.items():
+        sets[name] = lines('{}/{}.txt'.format(repo, name), issuetype)
+    for name in checkers.keys():
+        other1, other2 = others(name)
+        present = sets[name] - (sets[other1] | sets[other2])
+        if present:
+            print('Only present in {}:'.format(name))
+            for line in present:
+                print(line)
+            print()
+        missing = (sets[other1] & sets[other2]) - sets[name]
+        if missing:
+            print('Only missing in {}:'.format(name))
+            for line in missing:
+                print(line)
+            print()
+
+if len(sys.argv) < 2:
+    print('Please specify a repository.')
+else:
+    choice = sys.argv[1]
+    if choice not in repos:
+        print('Your choice is not one of the choices.')
+    else:
+        explain(choice)
---
+++ explain.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+python3 explain.py AutoDispose > AutoDispose/explain.txt
+python3 explain.py butterknife > butterknife/explain.txt
+python3 explain.py caffeine > caffeine/explain.txt
+python3 explain.py ColdSnap > ColdSnap/explain.txt
+python3 explain.py filesystem-generator > filesystem-generator/explain.txt
+python3 explain.py FloatingActionButtonSpeedDial > FloatingActionButtonSpeedDial/explain.txt
+python3 explain.py keyvaluestore > keyvaluestore/explain.txt
+python3 explain.py OANDAFX > OANDAFX/explain.txt
+python3 explain.py skaffold-tools-for-java > skaffold-tools-for-java/explain.txt
+python3 explain.py uLeak > uLeak/explain.txt
